2543e5396c57a96db52d9f1e159edc1eca04dffd
kyungseopk1m <kks0919@kakao.com>
2023-06-25 20:54:49 +0900

    add 고양이 데이터 모킹

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,12 +1,16 @@
 import * as express from 'express';
+import { Cat, CatType } from './app.model';
+
 const app: express.Express = express();
-const port: number = 8000;
+
+const data = [1, 2, 3, 4];
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  // res.send('Hello World!')
-  res.send({ name: 'nomad'})
+  console.log(req);
+  res.send({ cats: Cat })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(8000, () => {
+  console.log('server is on...');
+  
 })
