cba6fc31082432d42551fbe5a1381418ee3bf4fe
kyungseopk1m <kks0919@kakao.com>
2023-06-25 22:46:05 +0900

    learn express middleware

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,16 +1,37 @@
+// npm run start:dev
 import * as express from 'express';
 import { Cat, CatType } from './app.model';
 
 const app: express.Express = express();
 
-const data = [1, 2, 3, 4];
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log('this is logging middleware');
+  next();
+});
+
+app.get('/cats/som', (req, res, next) => {
+  console.log('this is som middleware');
+  next();
+});
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  console.log(req);
-  res.send({ cats: Cat })
-})
+  res.send({ cats: Cat });
+});
+
+app.get('/cats/blue', (req, res, next: express.NextFunction) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get('/cats/som', (req, res) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log('this is error middleware');
+  res.send({ error: '404 not found error' });
+});
 
 app.listen(8000, () => {
   console.log('server is on...');
-  
-})
+});
