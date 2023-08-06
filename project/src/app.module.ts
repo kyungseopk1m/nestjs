4470d6213634cfffe213bf5f5bbc99c9b3b73267
kyungseopk1m <kks0919@kakao.com>
2023-08-06 23:40:52 +0900

    ➖

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -1,25 +1,17 @@
+// import { CatsService } from './cats/cats.service';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import * as mongoose from 'mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+// import { CatsModule } from './cats/cats.module';
+// import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI),
-    CatsModule,
-  ],
+  // imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
-  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
-    mongoose.set('debug', this.isDev);
+    // consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
--- project/src/main.ts
+++ project/src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+// import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new HttpExceptionFilter());
-  const PORT = process.env.PROT;
-  await app.listen(PORT);
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(8000);
 }
+
 bootstrap();
