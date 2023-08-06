ac5adda95b0bb4435f91f995ab121a5775f3075b
kyungseopk1m <kks0919@kakao.com>
2023-08-06 23:08:04 +0900

    ✨ add mongoose modules

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
