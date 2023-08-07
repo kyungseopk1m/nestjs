ac5aabf9bfcebaa890b4da956945e1bc6d69205b
kyungseopk1m <kks0919@kakao.com>
2023-08-08 01:54:31 +0900

    🎉

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { AppService } from './app.service';
-// import { CatsService } from './cats/cats.service';
+import { CatsService } from './cats/cats.service';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService, // private readonly catsService: CatsService,
+    private readonly appService: AppService,
+    private readonly catsService: CatsService,
   ) {}
 
   @Get()
