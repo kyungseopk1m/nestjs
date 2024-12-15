9b7bac5473b6e5a089e0c25fc68417f58bbf014d
kyungseopk1m <kks0919@kakao.com>
2024-12-15 20:33:32 +0900

    ✨ chapter15 Pipe

--- cf_sns/src/posts/posts.controller.ts
+++ cf_sns/src/posts/posts.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Post,
   Put,
 } from '@nestjs/common';
@@ -24,8 +26,8 @@ export class PostsController {
   // id에 해당하는 post를 가져온다.
   // 예를 들어서 id=1일 경우 id가 1인 post를 가져온다.
   @Get(':id')
-  getPost(@Param('id') id: string) {
-    return this.postsService.getPostById(+id);
+  getPost(@Param('id', ParseIntPipe) id: number) {
+    return this.postsService.getPostById(id);
   }
 
   // 3) POST /posts
@@ -43,17 +45,17 @@ export class PostsController {
   // id에 해당하는 post를 변경한다.
   @Put(':id')
   putPost(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    return this.postsService.updatePost(+id, title, content);
+    return this.postsService.updatePost(id, title, content);
   }
 
   // 5) DELETE /posts/:id
   // id에 해당하는 post를 삭제한다.
   @Delete(':id')
-  deletePost(@Param('id') id: string) {
-    return this.postsService.deletePost(+id);
+  deletePost(@Param('id', ParseIntPipe) id: number) {
+    return this.postsService.deletePost(id);
   }
 }
