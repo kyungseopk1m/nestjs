a29c1de4a2bd1f6106037db8a7949ec8e4fd06f5
kyungseopk1m <kks0919@kakao.com>
2024-12-18 22:28:01 +0900

    ✨ chapter22 Class Transformer

--- cf_sns/src/users/entities/users.entity.ts
+++ cf_sns/src/users/entities/users.entity.ts
@@ -18,6 +18,7 @@ import {
 import { lengthValidationMessage } from 'src/common/validation-message/length-validation.message';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { emailValidationMessage } from 'src/common/validation-message/email-validation.message';
+import { Exclude } from 'class-transformer';
 
 /**
  *  id: number
@@ -70,6 +71,20 @@ export class UsersModel extends BaseModel {
   @Length(3, 8, {
     message: lengthValidationMessage,
   })
+  /**
+   * frontend -> backend
+   * plain object (JSON) -> class instance (dto)
+   *
+   * Response
+   * backend -> frontend
+   * class instance (dto) -> plain object (JSON)
+   *
+   * toClassOnly -> class instance 변환될때만
+   * toPlainOnly -> plain object로 변환될때만
+   */
+  // @Exclude({
+  //   toPlainOnly: true,
+  // })
   password: string;
 
   @Column({
