cb90a27083af69d0de6b5c7ec6d04bab58b36d4f
kyungseopk1m <kks0919@kakao.com>
2025-01-01 17:41:43 +0900

    ✨ chapter28 Static File Serving

--- cf_sns/src/app.module.ts
+++ cf_sns/src/app.module.ts
@@ -17,10 +17,19 @@ import {
   ENV_DB_USERNAME_KEY,
 } from './common/const/env-keys.const';
 import { ENV_DB_HOST_KEY } from './common/const/env-keys.const';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PUBLIC_FOLDER_PATH } from './common/const/path.const';
 
 @Module({
   imports: [
     PostsModule,
+    ServeStaticModule.forRoot({
+      // 4022.jpg
+      // http://localhost:3000/public/posts/4022.jpg
+      // http://localhost:3000/posts/4022.jpg
+      rootPath: PUBLIC_FOLDER_PATH,
+      serveRoot: '/posts',
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
