66dfb2866917cec2935d19964759828de9d5d29b
kyungseopk1m <kks0919@kakao.com>
2025-09-14 18:55:30 +0900

    ✨ chapter35 SocketIO advanced

--- cf_sns/src/chats/messages/dto/create-messages.dto.ts
+++ cf_sns/src/chats/messages/dto/create-messages.dto.ts
@@ -5,8 +5,4 @@ import { IsNumber } from 'class-validator';
 export class CreateMessagesDto extends PickType(MessagesModel, ['message']) {
   @IsNumber()
   chatId: number;
-
-  // 임시로 number
-  @IsNumber()
-  authorId: number;
 }
--- cf_sns/src/users/entities/users.entity.ts
+++ cf_sns/src/users/entities/users.entity.ts
@@ -86,9 +86,9 @@ export class UsersModel extends BaseModel {
    * toClassOnly -> class instance 변환될때만
    * toPlainOnly -> plain object로 변환될때만
    */
-  // @Exclude({
-  //   toPlainOnly: true,
-  // })
+  @Exclude({
+    toPlainOnly: true,
+  })
   password: string;
 
   @Column({
